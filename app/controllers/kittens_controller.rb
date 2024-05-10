ab96033da5c7ca4bcbb10570524b3e69dbe63f78
hellosamm <142949327+hellosamm@users.noreply.github.com>
2024-05-10 11:25:43 -1000

    pushing to github so I can continue working remotely

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,4 +1,8 @@
 class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+  
   def new
     @kitten = Kitten.new
   end
@@ -6,4 +10,54 @@ class KittensController < ApplicationController
   def edit
     @kitten = Kitten.find(params[:id])
   end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end 
+end
+
+def show
+  @kitten = Kitten.find(params[:id])
+end
+
+def update
+  @kitten = Kitten.find(params[:id])
+
+  if @kitten.update(kitten_params)
+    flash[:notice] = "kitten was updated successfully"
+    redirect_to @kitten
+  else
+    render "edit"
+  end
+end 
+# shows exact same code for updating a form
+# https://stackoverflow.com/questions/42844615/rails-update-method-not-saving-form-data
+
+
+def destroy
+  @kitten = Kitten.find(params[:id])
+
+  @kitten.destroy
+  # if @kitten.destroy 
+  #   flash[:success] = "kitten was deleted"
+  # else
+  #   flash[:error] = "failed to delete"
+  # end
+  
+  redirect_to root_path
+
+end
+
+
+
+private
+
+def kitten_params
+  params.require(:kitten).permit(:name, :age, :cuteness, :softness)
 end
